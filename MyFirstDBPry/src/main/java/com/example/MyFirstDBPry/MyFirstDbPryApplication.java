ce287193ce77e453e7064d466fecd0b2c5109819
zzzam21 <jonas01311@gmail.com>
2026-02-18 08:14:21 -0500

    Add Project Spring Boot

---
+++ MyFirstDBPry/src/main/java/com/example/MyFirstDBPry/MyFirstDbPryApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstDBPry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstDbPryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstDbPryApplication.class, args);
+	}
+
+}
