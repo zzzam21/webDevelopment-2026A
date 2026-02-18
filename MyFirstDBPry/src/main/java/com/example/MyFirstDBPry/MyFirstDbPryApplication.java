607ed2f59df3d98f4a90d48cb7c473de52054aff
zzzam21 <jonas01311@gmail.com>
2026-02-18 08:43:26 -0500

    Changes in project for Visual Studio Code

--- MyFirstDBPry/src/main/java/com/example/MyFirstDBPry/MyFirstDbPryApplication.java
+++ MyFirstDBPry/src/main/java/com/example/MyFirstDBPry/MyFirstDbPryApplication.java
@@ -2,10 +2,16 @@ package com.example.MyFirstDBPry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MyFirstDbPryApplication {
-
+	@RequestMapping("/")
+	String home() {
+		return "Hello Word! Spring Boot 4.0.2 is here. Samuel Salazar";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstDbPryApplication.class, args);
 	}
