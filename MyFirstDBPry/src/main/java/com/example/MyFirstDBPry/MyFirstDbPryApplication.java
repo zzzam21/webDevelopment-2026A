698507affadd2200ad9e24d365eac445c3a4e2ad
zzzam21 <jonas01311@gmail.com>
2026-02-18 08:58:01 -0500

    feat: Project from Apache NetBeans

--- MyFirstDBPry/src/main/java/com/example/MyFirstDBPry/MyFirstDbPryApplication.java
+++ MyFirstDBPry/src/main/java/com/example/MyFirstDBPry/MyFirstDbPryApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @SpringBootApplication
 public class MyFirstDbPryApplication {
-	@RequestMapping("/")
+        @RequestMapping("/")
 	String home() {
-		return "Hello Word! Spring Boot 4.0.2 is here. Samuel Salazar. From Visual Studio";
+		return "Hello Word! Spring Boot 4.0.2 is here. Samuel Salazar. From Apache Netbeans";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstDbPryApplication.class, args);
